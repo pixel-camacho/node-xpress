f3f2df7f4ebbae124f42c6a7c9411801a0e3d5ef
pixel-camacho <dallan_888@hotmail.com>
2020-06-07 13:49:55 -0700

    agregando xpress

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
-var http = require('http');
-var {countries} =  require('countries-list');
-var url = require('url');
-var querystring = require('querystring');
+const  {countries} =  require('countries-list');
+const express = require('express');
 
-var server = http.createServer( function(request, response){
+const app = express();
 
-    var parsed = url.parse(request.url);
-    var pathname = parsed.pathname;
-    var query = querystring.parse(parsed.query);
-    console.log(query);
-
-if(pathname == '/')
+app.listen(4000,function()
 {
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.write('<html><head><title>Home</title></head><body><h1>Bienvenido</h1></body></html>');
-    response.end();
-}else if(pathname == '/exit')
-{
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.write('<html><head><title>Home</title></head><body><h1>Adios Usuario...</h1></body></html>');
-    response.end();
-}else if(pathname == '/country')
-{
-    response.writeHead(200,{'Content-Type': 'application/json'});
-    response.write(JSON.stringify(countries[query.code]));
-    response.end();
-}else
-{
-    response.writeHead(404,{'Content-Type': 'text/html'});
-    response.write('<html><head><title>Home</title></head><body><h1>NOT FOUNT </h1></body></html>');
-    response.end();
-}
-    
+  console.log('Server load');
 });
-server.listen(4000);
-console.log('Server load');
+
+app.get('/',function(request,response)
+{
+  response.status(200).send('Hello');
+});
+app.get('*',function(request,response)
+{
+ response.status(404).send('NOT FOUND');
+});
+
+
+// var server = http.createServer( function(request, response){
+
+//     var parsed = url.parse(request.url);
+//     var pathname = parsed.pathname;
+//     var query = querystring.parse(parsed.query);
+//     console.log(query);
+
+// if(pathname == '/')
+// {
+//     response.writeHead(200,{'Content-Type': 'text/html'});
+//     response.write('<html><head><title>Home</title></head><body><h1>Bienvenido</h1></body></html>');
+//     response.end();
+// }else if(pathname == '/exit')
+// {
+//     response.writeHead(200,{'Content-Type': 'text/html'});
+//     response.write('<html><head><title>Home</title></head><body><h1>Adios Usuario...</h1></body></html>');
+//     response.end();
+// }else if(pathname == '/country')
+// {
+//     response.writeHead(200,{'Content-Type': 'application/json'});
+//     response.write(JSON.stringify(countries[query.code]));
+//     response.end();
+// }else
+// {
+//     response.writeHead(404,{'Content-Type': 'text/html'});
+//     response.write('<html><head><title>Home</title></head><body><h1>NOT FOUNT </h1></body></html>');
+//     response.end();
+// }
+    
+// });
+// server.listen(4000);
+// console.log('Server load');
 
 
